7af2ec4c7e7750c523e38479d5c5b2c71913682e
usernamezero00 <marselsavdalyan95@inbox.ru>
2025-11-15 17:17:49 +0400

    🔧 Update deploy action: save private key to S3 securely, remove local copy; update EC2 module for workspace integration

--- modules/ec2_instance/outputs.tf
+++ modules/ec2_instance/outputs.tf
@@ -34,3 +34,11 @@ output "key_name" {
   description = "Name of the generated key pair"
   value       = aws_key_pair.vpn_key.key_name
 }
+
+#---------------------------------------------------------------------------------------
+# VPN server public IP
+#---------------------------------------------------------------------------------------
+output "vpn_server_public_ip" {
+  value       = aws_instance.vpn_server.public_ip
+  description = "Public IP of the deployed VPN server"
+}
